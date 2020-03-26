ac21c0f57a633cc810d2526713de6cd2f14b8581
Susan Weaver <susan@weaverlogic.com>
2020-03-26 13:46:27 -0700

    gladiator game 3.2.4 known working code

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,7 +6,13 @@ var playerMoney = 10;
 // You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
+console.log(enemyNames);
+console.log(enemyNames[0]);
+console.log(enemyNames[1]);
+console.log(enemyNames[2]);
+console.log(enemyNames.length);
+
 var enemyHealth = 50;
 var enemyAttack = 12;
 
@@ -24,21 +30,21 @@ var fight = function() {
       enemyHealth = enemyHealth - playerAttack;
       // 3.1.7 Log a resulting message to the console so we know that it worked.
       console.log(
-        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+        playerName + " attacked " + enemyNames + ". " + enemyNames + " now has " + enemyHealth + " health remaining."
       );
 
             // 3.1.7 check enemy's health
             if (enemyHealth <= 0) {
-            window.alert(enemyName + " has died!");
+            window.alert(enemyNames + " has died!");
             } else {
-            window.alert(enemyName + " still has " + enemyHealth + " health left.");
+            window.alert(enemyNames + " still has " + enemyHealth + " health left.");
             }
 
       // 3.1.8 remove player's health by subtracting the amount set in the enemyAttack variable
       playerHealth = playerHealth - enemyAttack;
       // 3.1.7 Log a resulting message to the console so we know that it worked.
       console.log(
-        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+        enemyNames + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
       );
 
       //check player's health
